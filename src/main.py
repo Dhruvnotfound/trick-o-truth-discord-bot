68c1de6009b147c6ae760e30e540861f34168f6d
Dhruvnotfound <dhruvgupta3107@gmail.com>
2024-07-20 22:22:33 +0530

    getenv

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import random
 import asyncio
 import os
+from dotenv import load_dotenv
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -341,5 +342,5 @@ async def end_game(ctx):
         await ctx.send(f"It's a tie! The winners are {winners_names} with {game.get_scores()[winners[0]]} points each! 🏆")
 
     game.reset()
-
-bot.run(os.environ['TOKEN'])
+load_dotenv()
+bot.run(os.getenv['TOKEN'])
